9b1b6907414cf4c3365e14e173d1fa8fb8d25d44
haldomae <hal.domae@gmail.com>
2024-07-22 11:50:54 +0900

    画像付きのListViewに変更

---
+++ app/src/main/java/com/hal_domae/dictionary/Datasource.kt
@@ -0,0 +1,12 @@
+package com.hal_domae.dictionary
+
+val listData = listOf(
+    mapOf("image" to R.drawable.monster01, "name" to "モンスター01", "explain" to "モンスター01の詳細"),
+    mapOf("image" to R.drawable.monster02, "name" to "モンスター02", "explain" to "モンスター02の詳細"),
+    mapOf("image" to R.drawable.monster03, "name" to "モンスター03", "explain" to "モンスター03の詳細"),
+    mapOf("image" to R.drawable.monster04, "name" to "モンスター04", "explain" to "モンスター04の詳細"),
+    mapOf("image" to R.drawable.monster05, "name" to "モンスター05", "explain" to "モンスター05の詳細"),
+    mapOf("image" to R.drawable.monster06, "name" to "モンスター06", "explain" to "モンスター06の詳細"),
+    mapOf("image" to R.drawable.monster07, "name" to "モンスター07", "explain" to "モンスター07の詳細"),
+    mapOf("image" to R.drawable.monster08, "name" to "モンスター08", "explain" to "モンスター08の詳細"),
+)
--- app/src/main/java/com/hal_domae/dictionary/MainActivity.kt
+++ app/src/main/java/com/hal_domae/dictionary/MainActivity.kt
@@ -2,6 +2,7 @@ package com.hal_domae.dictionary
 
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import android.widget.SimpleAdapter
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -37,10 +38,18 @@ class MainActivity : AppCompatActivity() {
         // 1つ目の引数thisはコンテキスト(アプリやアクティビティの情報)
         // 2つ目の引数はリストの各項目に使うレイアウト(R.layout.simple_expandable_list_item_1はテキスト項目が1つだけあるレイアウト)
         // 3つ目の引数はリストに表示するデータ
-        binding.list.adapter = ArrayAdapter(
+//        binding.list.adapter = ArrayAdapter(
+//            this,
+//            android.R.layout.simple_expandable_list_item_1,
+//            data
+//        )
+
+        binding.list.adapter = SimpleAdapter(
             this,
-            android.R.layout.simple_expandable_list_item_1,
-            data
+            listData,
+            R.layout.list_itme,
+            arrayOf("image", "name"),
+            intArrayOf(R.id.image, R.id.name)
         )
 
         // 項目をタップした時のリスナー
@@ -48,7 +57,8 @@ class MainActivity : AppCompatActivity() {
         // Toastは画面下部に表示される簡易的なメッセージ
         // Toastの引数は1:コンテキスト、2:表示する文字列、3:表示時間(Toast.LENGTH_SHORTは短い)
         binding.list.setOnItemClickListener { parent, view, position, id ->
-            Toast.makeText(this@MainActivity, "{data[position]を選択しました}", Toast.LENGTH_SHORT).show()
+            //Toast.makeText(this@MainActivity, "{data[position]を選択しました}", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this,"${listData[position]["name"]}を選択しました", Toast.LENGTH_SHORT).show()
         }
     }
 }
